bf415e4a5ece1ef16269ee403ba5cc8685bd63e5
reneuam <renet@uassistme.com>
2022-08-23 13:09:38 -0600

    Add database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BalanceModule } from './api/balance/balance.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: process.env.DATABASE_TYPE as any || 'mysql',
+      host: process.env.DATABASE_HOST || 'localhost',
+      port: parseInt(process.env.DATABASE_PORT) || 3306,
+      username  : process.env.DATABASE_USERNAME || 'root',
+      password  : process.env.DATABASE_PASSWORD || 'password',
+      database: process.env.DATABASE || 'timeoff',
+      entities: [],
+      autoLoadEntities: true,
+      synchronize: false
+    }),
+    BalanceModule
+  ],
 })
 export class AppModule {}
