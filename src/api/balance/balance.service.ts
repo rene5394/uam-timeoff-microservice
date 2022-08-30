b23a6e94edd02e25893f102bc5386bb92e38b202
reneuam <renet@uassistme.com>
2022-08-30 16:09:51 -0600

    Add validation for update vacation and comp days

--- src/api/balance/balance.service.ts
+++ src/api/balance/balance.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { BalanceOperation } from '../../common/enums/balanceOperation.enum';
 import { CreateBalanceDto } from './dto/create-balance.dto';
 import { UpdateBalanceDto } from './dto/update-balance.dto';
 import { Balance } from './entities/balance.entity';
@@ -34,5 +35,47 @@ export class BalanceService {
 
   remove(id: number) {
     return `This action removes a #${id} balance`;
+  }
+
+  async validateCompDaysUpdate(id: number, operation:number, compDays: number): Promise<any> {
+    const balance  = await this.findOne(id);
+    let totalCompDays: number;
+
+    if (operation === BalanceOperation.addition) {
+      totalCompDays = balance.compDays + compDays;
+    }
+    
+    if (operation === BalanceOperation.substraction) {
+      if (compDays > balance.compDays) {
+        return {
+          error: 'Amount of comp days must not be more of available comp days'
+        };
+      }
+
+      totalCompDays = balance.compDays - compDays;
+    }
+
+    return { comDays: totalCompDays, vacationDays: balance.vacationDays };
+  }
+
+  async validateVacationsUpdate(id: number, operation: number, vacationDays: number): Promise<any> {
+    const balance = await this.findOne(id);
+    let totalVacationDays: number;
+
+    if (operation === BalanceOperation.addition) {
+      totalVacationDays = balance.vacationDays + vacationDays;
+    }
+    
+    if (operation === BalanceOperation.substraction) {
+      if (vacationDays > balance.vacationDays) {
+        return {
+          error: 'Amount of vacations must not be greater then available vacations'
+        };
+      }
+
+      totalVacationDays = balance.vacationDays - vacationDays;
+    }
+
+    return { compDays: balance.compDays, vacationDays: totalVacationDays };
   }
 }
