2f68a8a751382336a568fc68fe9f92999015cb6c
reneuam <renet@uassistme.com>
2022-08-31 16:47:11 -0600

    Add daysAfterRequest, MaxDaysRequested and MaxRequestsByDay

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -5,3 +5,20 @@ export enum RabbitMQ {
     StatusQueue = 'timeoffStatus',
     TypeQueue = 'timeoffType',
 }
+
+export class DaysAfterRequest {
+    static readonly minDaysCompDays = 15;
+    static readonly minDaysVacations = 30;
+    static readonly maxMonths = 6;
+}
+
+export class MaxDaysRequested {
+    static readonly compDays = 15;
+    static readonly vacations = 15;
+    static readonly licenciaExtraordinaria = 15;
+}
+
+export class MaxRequestsByDay {
+    static readonly compDays = 25;
+    static readonly vacations = 25;
+}
