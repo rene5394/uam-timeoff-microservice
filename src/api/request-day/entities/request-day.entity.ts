e8ee8675ed4aae07f22d3c0e668ebc55cadbbc7c
reneuam <renet@uassistme.com>
2022-08-31 15:28:48 -0600

    Set request day entity

--- src/api/request-day/entities/request-day.entity.ts
+++ src/api/request-day/entities/request-day.entity.ts
@@ -1,1 +1,22 @@
-export class RequestDay {}
+import { Column, Entity, PrimaryColumn } from "typeorm";
+
+@Entity('requestDays')
+export class RequestDay {
+    @PrimaryColumn({ type: 'bigint', nullable: false})
+    id: number;
+  
+    @Column({ type: 'bigint', nullable: false })
+    requestId: number;
+
+    @Column({ type: 'datetime', nullable: false })
+    day: Date;
+
+    @Column({ type: 'tinyint', nullable: false })
+    admin: number;
+
+    @Column({ type: 'datetime' })
+    createdAt: Date;
+
+    @Column({ type: 'datetime' })
+    updatedAt: Date;
+}
