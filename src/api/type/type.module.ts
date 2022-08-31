6f9cd472bcde0f733297470597f45d008cf87219
reneuam <renet@uassistme.com>
2022-08-31 00:35:43 -0600

    Add repository for status and type module

--- src/api/type/type.module.ts
+++ src/api/type/type.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeService } from './type.service';
 import { TypeController } from './type.controller';
+import { Type } from './entities/type.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Type])],
   controllers: [TypeController],
   providers: [TypeService]
 })
