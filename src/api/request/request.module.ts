8f54e6616681e5e5db3e84e1e60e2b50ea9eefd6
reneuam <renet@uassistme.com>
2022-08-31 15:41:26 -0600

    Import request day module

--- src/api/request/request.module.ts
+++ src/api/request/request.module.ts
@@ -3,9 +3,10 @@ import { RequestService } from './request.service';
 import { RequestController } from './request.controller';
 import { Request } from './entities/request.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { RequestDayModule } from '../request-day/request-day.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Request])],
+  imports: [TypeOrmModule.forFeature([Request]), RequestDayModule],
   controllers: [RequestController],
   providers: [RequestService]
 })
