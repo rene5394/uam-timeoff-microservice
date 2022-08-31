bb84f42d62ee8e6beba81dd89185538bfe6c989f
reneuam <renet@uassistme.com>
2022-08-31 15:39:03 -0600

    Add request day typeorm

--- src/api/request-day/request-day.module.ts
+++ src/api/request-day/request-day.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { RequestDay } from './entities/request-day.entity';
 import { RequestDayService } from './request-day.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([RequestDay])],
   providers: [RequestDayService],
   exports: [RequestDayService]
 })
