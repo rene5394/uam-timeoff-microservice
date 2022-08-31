19e87f19ee2321543dbde71e685023b1736e07b0
reneuam <renet@uassistme.com>
2022-08-31 00:39:59 -0600

    Set findAll and FindOne for status service

--- src/api/status/status.service.ts
+++ src/api/status/status.service.ts
@@ -1,26 +1,20 @@
 import { Injectable } from '@nestjs/common';
-import { CreateStatusDto } from './dto/create-status.dto';
-import { UpdateStatusDto } from './dto/update-status.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Status } from './entities/status.entity';
 
 @Injectable()
 export class StatusService {
-  create(createStatusDto: CreateStatusDto) {
-    return 'This action adds a new status';
+  constructor(
+    @InjectRepository(Status)
+    private readonly statusRepository: Repository<Status>
+  ) {}
+
+  async findAll(): Promise<Status[]> {
+    return await this.statusRepository.find();
   }
 
-  findAll() {
-    return `This action returns all status`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} status`;
-  }
-
-  update(id: number, updateStatusDto: UpdateStatusDto) {
-    return `This action updates a #${id} status`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} status`;
+  async findOne(id: number): Promise<Status> {
+    return await this.statusRepository.findOne({ where: { id: id } });
   }
 }
