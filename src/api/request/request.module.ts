efe2f85e8d449e0e1a5379ed32cab796a1bdba33
reneuam <renet@uassistme.com>
2022-08-31 15:43:29 -0600

    Import balance module

--- src/api/request/request.module.ts
+++ src/api/request/request.module.ts
@@ -4,9 +4,10 @@ import { RequestController } from './request.controller';
 import { Request } from './entities/request.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RequestDayModule } from '../request-day/request-day.module';
+import { BalanceModule } from '../balance/balance.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Request]), RequestDayModule],
+  imports: [TypeOrmModule.forFeature([Request]), BalanceModule, RequestDayModule],
   controllers: [RequestController],
   providers: [RequestService]
 })
