996f38314e9b39f4856c2f4dd8097fc7c2d6f608
reneuam <renet@uassistme.com>
2022-09-01 15:44:23 -0600

    Add typeOrModule to to transaction and transaction status

--- src/api/transaction-status/transaction-status.module.ts
+++ src/api/transaction-status/transaction-status.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TransactionStatusService } from './transaction-status.service';
 import { TransactionStatusController } from './transaction-status.controller';
+import { TransactionStatus } from './entities/transaction-status.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([TransactionStatus])],
   controllers: [TransactionStatusController],
   providers: [TransactionStatusService]
 })
