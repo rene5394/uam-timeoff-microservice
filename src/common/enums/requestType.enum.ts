60742ac201e8e364f0c86b4b4d0ca71aca15452c
reneuam <renet@uassistme.com>
2022-10-28 10:36:46 -0600

    Fix identation

--- src/common/enums/requestType.enum.ts
+++ src/common/enums/requestType.enum.ts
@@ -1,7 +1,7 @@
 export enum RequestType {
-    compDay = 1,
-    vacation = 2,
-    licenciaExtraordinaria = 3,
-    medicalLeave = 4,
-    permisoSinGoce = 5
+  compDay = 1,
+  vacation = 2,
+  licenciaExtraordinaria = 3,
+  medicalLeave = 4,
+  permisoSinGoce = 5
 };
