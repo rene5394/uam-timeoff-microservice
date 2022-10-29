ba4275989c40fe512e5305063406823a65f54456
reneuam <renet@uassistme.com>
2022-10-29 10:27:07 -0600

    Add timeout global interceptor globally

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 import { RabbitMQ } from './common/constants';
+import { TimeOutInterceptor } from './common/interceptors/timeout.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.createMicroservice(AppModule, {
@@ -11,6 +12,7 @@ async function bootstrap() {
         queue: RabbitMQ.TimeOffQueue
       },
     });
+  app.useGlobalInterceptors(new TimeOutInterceptor);
   await app.listen();
 }
 bootstrap();
