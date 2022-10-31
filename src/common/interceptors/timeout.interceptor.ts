03aa9c16eb8a46970cf79c25d5f72cbff1a05cfa
reneuam <renet@uassistme.com>
2022-10-31 11:40:12 -0600

    Add RPC exception return on timeout error

--- src/common/interceptors/timeout.interceptor.ts
+++ src/common/interceptors/timeout.interceptor.ts
@@ -1,6 +1,5 @@
-import { CallHandler, ExecutionContext, HttpStatus, Injectable, NestInterceptor } from '@nestjs/common'
-import { RpcException } from '@nestjs/microservices';
-import { Observable, throwError, TimeoutError } from 'rxjs';
+import { CallHandler, ExecutionContext, HttpStatus, Injectable, NestInterceptor } from '@nestjs/common';
+import { Observable, TimeoutError } from 'rxjs';
 import { catchError, timeout } from 'rxjs/operators';
 import { CustomRpcException } from '../exception/custom-rpc.exception';
 
@@ -8,8 +7,6 @@ import { CustomRpcException } from '../exception/custom-rpc.exception';
 export class TimeOutInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler<any>)
   : Observable<any> | Promise<Observable<any>> {
-    console.log('This is an interceptor time off microservices');
-    
     return next.handle().pipe(
       timeout(30000),
       catchError(err => {
