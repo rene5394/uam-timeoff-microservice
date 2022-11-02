1554a94ef946bb9fb3877677cee0e52adbdb935c
reneuam <renet@uassistme.com>
2022-11-02 12:03:05 -0600

    Change status names from created, approved and denied by coach for created, approved and denied by tl

--- src/common/enums/transactionStatus.enum.ts
+++ src/common/enums/transactionStatus.enum.ts
@@ -1,11 +1,11 @@
 export enum TransactionStatus {
   createdByBP = 1,
-  createdByCoach = 2,
+  createdByTL = 2,
   createdByAdmin = 3,
   createdByHR = 4,
-  approvedByCoach = 5,
+  approvedByTL = 5,
   approvedByHR = 6,
-  deniedByCoach = 7,
+  deniedByTL = 7,
   deniedByHR = 8,
   cancelledByBP = 9,
   cancelledByHR = 10
